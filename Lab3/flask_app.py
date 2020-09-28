185a11b0d6d0543eb0b596047a29d3e5ae1973bf
Tabea Haverkamp <tabea@haverkamp-os.de>
2020-09-28 21:02:54 +0000

    working flask

--- Lab3/flask_app.py
+++ Lab3/flask_app.py
@@ -1,25 +1,15 @@
 #!flask/bin/python
 from flask import Flask, jsonify
-from flas_restful import Api, Resource, reqparse
+#from flas_restful import Api, Resource, reqparse
 import subprocess
 import sys
 
 app = Flask(__name__)
 
 
-@app.route('/cowsay/api/v1.0/saysomething', methods=['GET'])
-def cow_say():
-    data=subprocess.check_output(["cowsay","Hello student"])
-    return data
-
-if __name__ == '__main__':
-    
-    app.run(host='0.0.0.0',debug=True)
-
-
-@app.route('My/weird/path/to/HAPPINESS', methods=['GET'])
+@app.route('/pronouns', methods=['GET'])
 def twitter_count():
-    data=subprocess.check_output(["cowsay","Hello student"])
+    data=subprocess.check_output(["python3","calling_celery.py"])
     return data
 
 if __name__ == '__main__':
