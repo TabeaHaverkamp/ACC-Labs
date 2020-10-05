de9c0b7c1707598ce084950c72a6ed880314abed
Tabea Haverkamp <tabea@haverkamp-os.de>
2020-10-05 09:38:37 +0000

    trying to use celery

--- Lab3/calling_celery.py
+++ Lab3/calling_celery.py
@@ -1,9 +1,13 @@
 from tasks import get_lines
+import celery
 import time
 t = []
-for i in range(10):
-    start = time.time()
-    print(get_lines.delay('/home/ubuntu/ACC-Labs/Lab3/data'))
-    t.append(time.time() - start)
 
-print(t)
+def on_raw_message(body):
+    print(body)
+# [get_lines('/home/ubuntu/ACC-Labs/Lab3/data') for x in range(5)]
+job = celery.group([get_lines('/home/ubuntu/ACC-Labs/Lab3/data'), get_lines('/home/ubuntu/ACC-Labs/Lab3/data')])
+start = time.time()
+job_result = job.delay()
+results = job_result.get() # this will block until the tasks finish but it wont deadlock
+print(time.time()-start)
