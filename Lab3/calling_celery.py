a6af2ee41a6f20d71e8bd925a6236cfa3beee5ee
Tabea Haverkamp <tabea@haverkamp-os.de>
2020-10-05 14:08:45 +0000

    add main function

--- Lab3/calling_celery.py
+++ Lab3/calling_celery.py
@@ -1,6 +1,7 @@
-from tasks import get_lines, count_pronouns
+from tasks import get_lines, count_pronouns, count_in_file
 from celery import group
 import time
+import os
 def add_dicts(dicts):
     dd = {}
     for d in dicts:
@@ -8,12 +9,20 @@ def add_dicts(dicts):
             if not key in dd: dd[key] = value
             dd[key] = dd[key] + value
     return dd
-dir = '/home/ubuntu/ACC-Labs/Lab3/data'
 
-start = time.time()
-job_result = count_pronouns(dir)
-#print the result
-results = job_result.get()
-print(results) 
-#print(add_dicts(results))
-print(start-time.time())
+
+def main():
+    directory = '/home/ubuntu/ACC-Labs/Lab3/datatest'
+    p = ["den", "det", "denna", "denne","han", "hon",  "hen"]
+    job = group(count_in_file.s(directory + os.sep + filename, p) for filename in os.listdir(directory))
+    job_result = job.apply_async()
+    start = time.time()
+    #job_result = count_pronouns(dir)
+    #print the result
+    results = job_result.get()
+    print(results) 
+    print(add_dicts(results))
+    print(time.time()-start)
+
+
+main()
