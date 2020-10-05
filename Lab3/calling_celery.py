68feb85c831b0efffa22b03f4d803c0fecc6be78
Tabea Haverkamp <tabea@haverkamp-os.de>
2020-10-05 13:28:19 +0000

    combine the dicts

--- Lab3/calling_celery.py
+++ Lab3/calling_celery.py
@@ -1,15 +1,19 @@
 from tasks import get_lines, count_pronouns
 from celery import group
 import time
-
+def add_dicts(dicts):
+    dd = {}
+    for d in dicts:
+        for key, value in d.items():
+            if not key in dd: dd[key] = value
+            dd[key] = dd[key] + value
+    return dd
 dir = '/home/ubuntu/ACC-Labs/Lab3/data'
-# build groups to be executed
-#g = [get_lines.s('/home/ubuntu/ACC-Labs/Lab3/data') for x in range(5)]
-#job = group(g)
 
-#execute the group
-# job_result = job()
-job_result = count_pronouns.delay(dir)
+start = time.time()
+job_result = count_pronouns(dir)
 #print the result
-results = job_result.get() 
-print(results)
+results = job_result.get()
+print(results) 
+#print(add_dicts(results))
+print(start-time.time())
