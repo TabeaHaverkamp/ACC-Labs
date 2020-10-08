271a715f0d3b37a1a03309d1f01de06314107a89
Tabea Haverkamp <tabea@haverkamp-os.de>
2020-10-08 14:51:14 +0000

    open/close file

--- Lab3/tasks.py
+++ Lab3/tasks.py
@@ -6,22 +6,28 @@ import time
 
 broker_url = 'amqp://tabea:tabeapassword@localhost:5672/tabeavhost'
 app = Celery('tasks', backend='rpc://', broker=broker_url )
-
+@app.task
+def add_dicts(dicts):
+    dd = {}
+    for d in dicts:
+        for key, value in d.items():
+            if not key in dd: dd[key] = value
+            dd[key] = dd[key] + value
+    return dd
 
 
 @app.task
 def count_in_file(filename, p):
     pron_count = {key: 0 for key in p}
-    tweet_file = open(filename)
+    with tweet_file as open(filename):
+        tweets = (line.rstrip() for line in tweet_file)
+        tweets_json = (json.loads(line) for line in tweets if line)
 
-    tweets = (line.rstrip() for line in tweet_file)
-    tweets_json = (json.loads(line) for line in tweets if line)
-
-    for tweet in tweets_json:
-        # only consider non retweets
-        if "retweeted_status" not in tweet:
-            words = re.split('[. , ? !]', tweet['text'])
-            for w in words: 
-                if w.lower() in p: # if the word is a pronoun, add it to the count
-                    pron_count[w.lower()] += 1
+        for tweet in tweets_json:
+            # only consider non retweets
+            if "retweeted_status" not in tweet:
+                words = re.split('[. , ? !]', tweet['text'])
+                for w in words: 
+                    if w.lower() in p: # if the word is a pronoun, add it to the count
+                        pron_count[w.lower()] += 1
     return pron_count
